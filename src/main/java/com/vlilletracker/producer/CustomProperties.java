f0485d70a29317aeb3a825b060aa3994f03d800c
Nicolas Deman <nicolas.deman@hotmail.fr>
2024-03-25 12:11:25 +0100

    app properties configuration

---
+++ src/main/java/com/vlilletracker/producer/CustomProperties.java
@@ -0,0 +1,15 @@
+package com.vlilletracker.producer;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "com.vlilletracker.producer") //map with the application.properties file
+public class CustomProperties {
+
+    private String apiUrl;
+
+    private String applicationDesignation;
+}
